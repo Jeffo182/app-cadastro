0971e7177fe97a6bd1145f346b306aef22ba4b7a
Jefferson Felix <jefferson.felix182@gmail.com>
2022-09-27 18:43:43 -0300

    falta logica de adicionar mais de 1 cliente

--- src/components/ClientesCadastrados.js
+++ src/components/ClientesCadastrados.js
@@ -1,15 +1,40 @@
 //A página de Clientes cadastrados deverá listar todos os clientes. Caso não haja cliente, a mensagem “Nenhum cliente cadastrado” deve aparecer na tela, juntamente com um botão para ir à pagina de cadastro. Esse botão deve permanecer na tela mesmo caso existam clientes.
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import Cadastrovazio from "./Cadastrovazio";
 
 class ClientesCadastrados extends Component {
-
   render() {
+    const { name, email, age } = this.props;
     return (
       <>
-      <h1>Clientes Cadastrados</h1>
+      {
+        name === '' || email === '' || age === '' ? <Cadastrovazio /> :
+        <div>
+        <h1>Clientes Cadastrados</h1>
+        <div>
+          <p>
+            Nome:
+            {name}
+            </p>
+          <p>
+            E-mail:
+            {email}
+            </p>
+          <p>
+            Idade:
+            {age}
+          </p>
+        </div>
+        </div>
+  }
       </>
     );
   }
 }
 
-export default ClientesCadastrados;
+const mapStateToProps = ({ cadastro }) => ({
+  ...cadastro,
+});
+
+export default connect(mapStateToProps)(ClientesCadastrados);
